17deec30dafec01b8429f4a2c0b9d7976d588be8
kidkang <723616880@qq.com>
2019-07-01 21:04:57 +0800

    sign function

--- src/Sign.php
+++ src/Sign.php
@@ -1,19 +1,20 @@
-<?php 
+<?php
 namespace Yjtec\Support;
 
 class Sign
 {
-    public static function make($secret,$timeStamp,$params,$debug=false){
+    public static function make($secret, $timeStamp, $params, $debug = false)
+    {
         ksort($params);
         $paramstr = '';
         foreach ($params as $k => $v) {
             $paramstr .= "{$k}{$v}";
         }
 
-        $str = "{$secret}-{$paramstr}-{$timeStamp}";
+        $str        = "{$secret}-{$paramstr}-{$timeStamp}";
         $re['sign'] = md5($str);
 
-        if($debug){
+        if ($debug) {
             $re['paramstr'] = $str;
         }
         return $re;
