4072390b21ad7788fb6e5ce565f4552b86c8a83f
kidkang <723616880@qq.com>
2019-07-31 09:52:05 +0800

    修改curl请求问单例

--- example/index.php
+++ example/index.php
@@ -3,4 +3,7 @@ require __DIR__.'/../vendor/autoload.php';
 use Yjtec\Support\Curl;
 //echo Curl::get('http://www.baidu.com');
 echo Curl::post('http://www.baidu.com',['a'=>1,'b'=>2]);
+echo Curl::post('http://www.baidu.com',['a'=>1,'b'=>2]);
+echo Curl::post('http://www.baidu.com',['a'=>1,'b'=>2]);
+echo Curl::post('http://www.baidu.com',['a'=>1,'b'=>2]);
 ?>
--- src/Curl.php
+++ src/Curl.php
@@ -4,25 +4,37 @@ namespace Yjtec\Support;
 class Curl
 {
     private $method; // 请求方式
+    private static $instance = null;
     public function __construct($method)
     {
+        $this->ch     = curl_init();
         $this->method = $method;
     }
-    public function curlGet($url, $post_data = 0, $header =[])
+    private function __clone()
+    {}
+    public static function getInstance($method)
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER,$header);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        if ($this->method == 'post') {
-            curl_setopt($ch, CURLOPT_POST, 1);
-
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-        } elseif ($this->method == 'get') {
-            curl_setopt($ch, CURLOPT_HEADER, 0);
+        if (self::$instance instanceof self) {
+            curl_reset(self::$instance->ch);
+            return self::$instance;
+        } else {
+            self::$instance = new self($method);
+            return self::$instance;
         }
-        $output = curl_exec($ch);
-        curl_close($ch);
+    }
+    public function curlGet($url, $post_data = 0, $header = [])
+    {
+        curl_setopt($this->ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($this->ch, CURLOPT_URL, $url);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+        if ($this->method == 'post') {
+            curl_setopt($this->ch, CURLOPT_POST, 1);
+
+            curl_setopt($this->ch, CURLOPT_POSTFIELDS, $post_data);
+        } elseif ($this->method == 'get') {
+            curl_setopt($this->ch, CURLOPT_HEADER, 0);
+        }
+        $output = curl_exec($this->ch);
         return $output;
     }
 
@@ -30,13 +42,16 @@ class Curl
     //Curl::post($url,['asdf'=>1])
     public static function __callStatic($method, $arguments)
     {
-        $data = [];
+        $data   = [];
+        $header = [];
         if (count($arguments) == 1) {
             list($url) = $arguments;
+        } else if (count($arguments) == 3) {
+            list($url, $data, $header) = $arguments;
         } else {
-            list($url, $data,$header) = $arguments;
+            list($url, $data) = $arguments;
         }
-        $obj = new self($method);
-        return $obj->curlGet($url, $data,$header);
+        $obj = self::getInstance($method);
+        return $obj->curlGet($url, $data, $header);
     }
 }
